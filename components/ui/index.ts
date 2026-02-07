7cf6d0feb8ee2ae9cb9922fcdae656818c877e19
iammatthewhenry <matthew@iammatthewhenry.com>
2026-02-06 18:15:50 -0800

    Update index.ts

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,2 +1,16 @@
+// Icons
 export { ShoppingBasket } from './ShoppingBasket';
 export { DollarSign } from './DollarSign';
+
+// Layout Components  
+export { Container } from './Container';
+export { Section } from './Section';
+export { Card } from './Card';
+
+// Text Components
+export { SectionHeader } from './SectionHeader';
+export { PageHeader } from './PageHeader';
+
+// Interactive Components
+export { Button } from './Button';
+export { ReadMoreLink } from './ReadMoreLink';
