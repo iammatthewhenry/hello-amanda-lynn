58b8b165a816660e5ed3bff1fb7ea84ecea2446d
iammatthewhenry <matthew@iammatthewhenry.com>
2026-02-21 18:56:00 -0800

    buttons

--- components/share-bar.tsx
+++ components/share-bar.tsx
@@ -70,7 +70,7 @@ export function ShareBar({ title, description, imageUrl, showPrint = false }: Sh
         className="share-button"
         aria-label="Share on Pinterest"
       >
-        <svg className="w-[21px] h-[21px]" viewBox="0 0 24 24" fill="currentColor">
+        <svg className="w-[24px] h-[24px]" viewBox="0 0 24 24" fill="currentColor">
           <path d="M12 2C6.477 2 2 6.477 2 12c0 4.237 2.636 7.855 6.356 9.312-.088-.791-.167-2.005.035-2.868.181-.78 1.172-4.97 1.172-4.97s-.299-.6-.299-1.486c0-1.39.806-2.428 1.81-2.428.852 0 1.264.64 1.264 1.408 0 .858-.545 2.14-.828 3.33-.236.995.5 1.807 1.48 1.807 1.778 0 3.144-1.874 3.144-4.58 0-2.393-1.72-4.068-4.176-4.068-2.845 0-4.516 2.135-4.516 4.34 0 .859.331 1.781.745 2.281a.3.3 0 01.069.288l-.278 1.133c-.044.183-.145.223-.335.134-1.249-.581-2.03-2.407-2.03-3.874 0-3.154 2.292-6.052 6.608-6.052 3.469 0 6.165 2.473 6.165 5.776 0 3.447-2.173 6.22-5.19 6.22-1.013 0-1.965-.525-2.291-1.148l-.623 2.378c-.226.869-.835 1.958-1.244 2.621.937.29 1.931.446 2.962.446 5.523 0 10-4.477 10-10S17.523 2 12 2z"/>
         </svg>
       </a>
@@ -82,7 +82,7 @@ export function ShareBar({ title, description, imageUrl, showPrint = false }: Sh
         className="share-button"
         aria-label="Share on Facebook"
       >
-        <Facebook className="w-[21px] h-[21px]" />
+        <Facebook className="w-[24px] h-[24px]" />
       </a>
 
       <a
@@ -92,7 +92,7 @@ export function ShareBar({ title, description, imageUrl, showPrint = false }: Sh
         className="share-button"
         aria-label="Share on Twitter"
       >
-        <Twitter className="w-[21px] h-[21px]" />
+        <Twitter className="w-[24px] h-[24px]" />
       </a>
 
       <a
@@ -102,7 +102,7 @@ export function ShareBar({ title, description, imageUrl, showPrint = false }: Sh
         className="share-button"
         aria-label="Share on WhatsApp"
       >
-        <svg className="w-[21px] h-[21px]" viewBox="0 0 24 24" fill="currentColor">
+        <svg className="w-[24px] h-[24px]" viewBox="0 0 24 24" fill="currentColor">
           <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z"/>
         </svg>
       </a>
@@ -112,7 +112,7 @@ export function ShareBar({ title, description, imageUrl, showPrint = false }: Sh
         className="share-button"
         aria-label="Share via Email"
       >
-        <Mail className="w-[21px] h-[21px]" />
+        <Mail className="w-[24px] h-[24px]" />
       </a>
 
       <a
@@ -122,7 +122,7 @@ export function ShareBar({ title, description, imageUrl, showPrint = false }: Sh
         className="share-button"
         aria-label="Share on Reddit"
       >
-        <svg className="w-[21px] h-[21px]" viewBox="0 0 24 24" fill="currentColor">
+        <svg className="w-[24px] h-[24px]" viewBox="0 0 24 24" fill="currentColor">
           <path d="M12 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0zm5.01 4.744c.688 0 1.25.561 1.25 1.249a1.25 1.25 0 0 1-2.498.056l-2.597-.547-.8 3.747c1.824.07 3.48.632 4.674 1.488.308-.309.73-.491 1.207-.491.968 0 1.754.786 1.754 1.754 0 .716-.435 1.333-1.01 1.614a3.111 3.111 0 0 1 .042.52c0 2.694-3.13 4.87-7.004 4.87-3.874 0-7.004-2.176-7.004-4.87 0-.183.015-.366.043-.534A1.748 1.748 0 0 1 4.028 12c0-.968.786-1.754 1.754-1.754.463 0 .898.196 1.207.49 1.207-.883 2.878-1.43 4.744-1.487l.885-4.182a.342.342 0 0 1 .14-.197.35.35 0 0 1 .238-.042l2.906.617a1.214 1.214 0 0 1 1.108-.701zM9.25 12C8.561 12 8 12.562 8 13.25c0 .687.561 1.248 1.25 1.248.687 0 1.248-.561 1.248-1.249 0-.688-.561-1.249-1.249-1.249zm5.5 0c-.687 0-1.248.561-1.248 1.25 0 .687.561 1.248 1.249 1.248.688 0 1.249-.561 1.249-1.249 0-.687-.562-1.249-1.25-1.249zm-5.466 3.99a.327.327 0 0 0-.231.094.33.33 0 0 0 0 .463c.842.842 2.484.913 2.961.913.477 0 2.105-.056 2.961-.913a.361.361 0 0 0 .029-.463.33.33 0 0 0-.464 0c-.547.533-1.684.73-2.512.73-.828 0-1.979-.196-2.512-.73a.326.326 0 0 0-.232-.095z"/>
         </svg>
       </a>
@@ -132,7 +132,7 @@ export function ShareBar({ title, description, imageUrl, showPrint = false }: Sh
         className="share-button"
         aria-label="Copy link"
       >
-        {copied ? <Copy className="w-[21px] h-[21px]" /> : <LinkIcon className="w-[21px] h-[21px]" />}
+        {copied ? <Copy className="w-[24px] h-[24px]" /> : <LinkIcon className="w-[24px] h-[24px]" />}
       </button>
 
       {canShare && (
@@ -141,7 +141,7 @@ export function ShareBar({ title, description, imageUrl, showPrint = false }: Sh
           className="share-button"
           aria-label="Share"
         >
-          <Share2 className="w-[21px] h-[21px]" />
+          <Share2 className="w-[24px] h-[24px]" />
         </button>
       )}
     </div>
