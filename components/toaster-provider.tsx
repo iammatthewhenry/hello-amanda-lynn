6ccc5c0b8a54982a518f708edb2759b2a37b3dbd
iammatthewhenry <matthew@iammatthewhenry.com>
2026-02-23 12:36:02 -0800

    Create toaster-provider.tsx

---
+++ components/toaster-provider.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const Toaster = dynamic(
+  () => import('sonner').then((m) => ({ default: m.Toaster })),
+  { ssr: false }
+);
+
+export function ToasterProvider() {
+  return <Toaster />;
+}
