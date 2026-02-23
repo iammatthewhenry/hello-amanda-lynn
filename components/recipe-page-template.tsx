8593dc4159e767dda9a45fc04bc3e7b4e9ef9726
iammatthewhenry <matthew@iammatthewhenry.com>
2026-02-22 22:15:18 -0800

    Update recipe-page-template.tsx

--- components/recipe-page-template.tsx
+++ components/recipe-page-template.tsx
@@ -14,7 +14,7 @@ interface RecipePageTemplateProps {
 export function RecipePageTemplate({ children }: RecipePageTemplateProps) {
   return (
     <div
-      className="border border-border pb-8 px-[4vw] sm:pb-16 sm:px-8 lg:pb-24 lg:px-14 relative w-full"
+      className="border border-border mt-6 sm:mt-8 pb-8 px-[4vw] sm:pb-16 sm:px-8 lg:pb-24 lg:px-14 relative w-full"
       style={{
         backgroundColor: "#FCFCFC",
         backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='200' height='200'%3E%3Cfilter id='noise'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3CfeColorMatrix type='saturate' values='0'/%3E%3CfeComponentTransfer%3E%3CfeFuncA type='discrete' tableValues='0 0 0 0 0 0 1 0'/%3E%3C/feFuncA%3E%3C/feComponentTransfer%3E%3C/filter%3E%3Crect width='200' height='200' filter='url(%23noise)' opacity='0.23' fill='%23000000'/%3E%3C/svg%3E")`,
