9334b764abb57aaced8e16b7a45359dd72dc5a8e
iammatthewhenry <matthew@iammatthewhenry.com>
2026-02-23 16:41:07 -0800

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,30 +1,26 @@
-import type { OpenNextConfig } from '@opennextjs/cloudflare';
-
-const config: OpenNextConfig = {
+export default {
   default: {
     override: {
-      wrapper: 'cloudflare-node',
-      converter: 'edge',
-      proxyExternalRequest: 'fetch',
-      incrementalCache: 'dummy',
-      tagCache: 'dummy',
-      queue: 'dummy',
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
     },
   },
 
-  edgeExternals: ['node:crypto'],
+  edgeExternals: ["node:crypto"],
 
   middleware: {
     external: true,
     override: {
-      wrapper: 'cloudflare-edge',
-      converter: 'edge',
-      proxyExternalRequest: 'fetch',
-      incrementalCache: 'dummy',
-      tagCache: 'dummy',
-      queue: 'dummy',
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
     },
   },
 };
-
-export default config;
