7d9c29a581f4aa43b91319a92b2d0dc0dd5e35d6
iammatthewhenry <matthew@iammatthewhenry.com>
2026-02-23 17:02:38 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isCloudflare = process.env.CF_PAGES === "1";
-
 const nextConfig = {
-  ...(isCloudflare ? { output: "standalone" } : {}),
+  output: "standalone",
 };
 
 export default nextConfig;
