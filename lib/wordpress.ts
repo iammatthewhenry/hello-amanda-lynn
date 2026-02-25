e1d13faba69f2e6a2124989ae51bfeca66d21bb3
iammatthewhenry <matthew@iammatthewhenry.com>
2026-02-25 12:31:04 -0800

    Update wordpress.ts

--- lib/wordpress.ts
+++ lib/wordpress.ts
@@ -42,7 +42,6 @@ export async function fetchGraphQL<T>(
         query,
         variables: variables || {},
       }),
-      // @ts-expect-error - Next.js extends fetch with next property
       next: {
         revalidate,
       },
