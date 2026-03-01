054c8607274e5306ab51b1fcf738b8cff719d8ac
iammatthewhenry <matthew@iammatthewhenry.com>
2026-03-01 13:46:11 -0800

    Update recipeCategories.ts

--- lib/data/recipeCategories.ts
+++ lib/data/recipeCategories.ts
@@ -69,3 +69,5 @@ export const recipeCategories: RecipeCategory[] = [
       'https://images.unsplash.com/photo-1665061580738-87be64cbd049?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=1080',
   },
 ];
+
+
